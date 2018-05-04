2a721b6fe7358e3723d749d9a04a11fd605c703d
Matthew-Kanderka <kanderkam@hotmail.com>
2018-05-04 09:11:06 -0600

    added basic functionality

--- assests/js/todos.js
+++ assests/js/todos.js
@@ -1,13 +1,24 @@
 // check off specific todos by clicking
-$("li").click(function() {
+$("ul").on("click", "li", function() {
 	//if li is gray, turn it black else turn gray
 	$(this).toggleClass("completed");
 });
 
 //click on X to delete todo
-$("span").click(function(event){
+$("ul").on("click", "span", function(event){
 	$(this).parent().fadeOut(500, function(){
 		$(this).remove();
 	});
 	event.stopPropagation();
+});
+
+$("input[type='text']").keypress(function(event){
+	if(event.which === 13)
+	{
+		//grabbing new todo text
+		var todoText = $(this).val();
+		$(this).val("");
+		//create new li and add to ul
+		$("ul").append("<li><span>X</span> " + todoText + "</li>");
+	}
 });
